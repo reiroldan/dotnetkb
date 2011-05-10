5f6e95a6bcebe2747892a6f13e46d1bc89e79c11
Rei Roldan <raroldan@gmail.com>
2011-05-10 16:38:10 +0200

    Stuff

--- src/WebUI/Modules/MainModule.cs
+++ src/WebUI/Modules/MainModule.cs
@@ -1,3 +1,4 @@
+using DotNetKillboard.Reporting;
 using Nancy;
 
 namespace DotNetKillboard.Modules
@@ -13,25 +14,83 @@ namespace DotNetKillboard.Modules
 
     public class AllianceModule : NancyModule
     {
-        public AllianceModule()
-            : base("alliance/{id}") {
+        private readonly IReportingRepository _repository;
+
+        public AllianceModule(IReportingRepository repository)
+            : base(@"alliance/(?<id>[\d]+)") {
+
+            _repository = repository;
+
+            Get["/kills"] = x => {
+                //var kills = _repository.QueryFor<IAllianceKillsQuery>(q => q.Id = x.id).Execute();
+                return Request.Uri;
+            };
+
+            Get["/losses"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/corps"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/ships_weapons"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/systems"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/corp_kills"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/corp_loosses"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/corp_kills_class"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/corp_losses_class"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/pilot_kills"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/pilot_loosses"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/pilot_kills_class"] = x => {
+                return Request.Uri;
+            };
+
+            Get["/pilot_losses_class"] = x => {
+                return Request.Uri;
+            };
         }
     }
 
     public class CorporationModule : NancyModule
     {
-
         public CorporationModule()
-            : base("corp/{id}") {
+            : base(@"corp/(?<id>[\d]+)") {
 
+            Get["/"] = x => {
+                return 123;
+            };
         }
     }
 
     public class PilotModule : NancyModule
     {
         public PilotModule()
-            : base("pilot") {
-           
+            : base(@"pilot/(?<id>[\d]+)") {
         }
     }
 }
